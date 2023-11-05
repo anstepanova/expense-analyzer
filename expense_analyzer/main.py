33fe40d267a65133d3b6574dac421b9b6079b906
Anastasia Stepanova <nastasija.stepanova@gmail.com>
2023-11-05 15:07:46 +0100

    Added linters, docker container initialization and moved poetry files

--- expense_analyzer/main.py
+++ expense_analyzer/main.py
@@ -12,4 +12,7 @@ async def root():
 x = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+    uvicorn.run(
+        app,
+        host="127.0.0.1", port=8080,
+    )
