d4a29a4f5832be946d110d9a40a5c815f40f6db2
Anastasia Stepanova <nastasija.stepanova@gmail.com>
2023-11-22 15:42:36 +0100

    Added pk to user table

--- expense_analyzer/models/user.py
+++ expense_analyzer/models/user.py
@@ -1,6 +1,6 @@
 """Describe User table."""
 
-from sqlalchemy import Column, String
+from sqlalchemy import Column, Integer, String
 
 from expense_analyzer.constants import DbConstants
 from expense_analyzer.models.mixins import TimestampMixin
@@ -8,5 +8,7 @@ from expense_analyzer.models.mixins import TimestampMixin
 
 class User(TimestampMixin, DbConstants.BASE):
     """Contain information about users."""
+
+    id = Column(Integer, primary_key=True)
     username = Column(String(length=256), nullable=False)
     email = Column(String(length=256), nullable=False, unique=True)
